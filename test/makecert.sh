e23a8be2f33ff75ee86123737e7bd6585094b0fd
Teemu Kataja <teemu.kataja@csc.fi>
2021-11-08 15:31:28 +0200

    Update test/makecert.sh
    Co-authored-by: Stefan Negru <blankdots@users.noreply.github.com>

--- test/makecert.sh
+++ test/makecert.sh
@@ -3,4 +3,4 @@
 # # if you need a new self-signed cert to test https
 openssl genrsa -out "tls.key" 4096
 openssl req -new -key "tls.key" -out "tls.csr" -subj '/C=FI/ST=Uusimaa/L=Espoo/O=CSC/CN=localhost'
-openssl x509 -req -days 365 -in "tls.csr" -signkey "tls.key" -out "tls.crt"
+openssl x509 -req -days 7300 -in "tls.csr" -signkey "tls.key" -out "tls.crt"
