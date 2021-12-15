936c5d34a46ee2b3c3cb6632f7dcda3435f4f0ae
Stefan Negru <stefan@blankdots.com>
2021-12-15 16:47:30 +0200

    add integration tests for all endpoints
    test POST gives 405
    test metadata datasets
    test downloading and decrypting files

--- .github/integration/tests/common/50_check_endpoint.sh
+++ .github/integration/tests/common/50_check_endpoint.sh
@@ -28,3 +28,77 @@ if [ "$check_401" != "401" ]; then
 fi
 
 echo "got correct response when no token provided"
+
+check_405=$(curl -o /dev/null -s -w "%{http_code}\n" -X POST --cacert certs/ca.pem https://localhost:443/metadata/datasets)
+
+if [ "$check_405" != "405" ]; then
+    echo "POST should not be allowed"
+    echo "got: ${check_405}"
+    exit 1
+fi
+
+echo "got correct response when POST method used"
+
+# Test good token
+
+token=$(curl "http://localhost:8000/tokens" | jq -r  '.[0]')
+
+## Test datasets endpoint
+
+check_dataset=$(curl --cacert certs/ca.pem -H "Authorization: Bearer $token" https://localhost:443/metadata/datasets | jq -r '.[0]')
+
+if [ "$check_dataset" != "https://doi.example/009/600.45" ]; then
+    echo "dataset https://doi.example/009/600.45 not found"
+    echo "got: ${check_dataset}"
+    exit 1
+fi
+
+echo "expected dataset found"
+
+## Test datasets/files endpoint 
+
+check_files=$(curl --cacert certs/ca.pem -H "Authorization: Bearer $token" "https://localhost:443/metadata/datasets/https://doi.example/009/600.45/files" | jq -r '.[0].fileId')
+
+if [ "$check_files" != "urn:neic:001-002" ]; then
+    echo "file with id urn:neic:001-002 not found"
+    echo "got: ${check_files}"
+    exit 1
+fi
+
+echo "expected file found"
+
+# Test file can be decrypted
+## test also the files endpoint
+
+C4GH_PASSPHRASE=$(grep -F passphrase config.yaml | sed -e 's/.* //' -e 's/"//g')
+export C4GH_PASSPHRASE
+
+crypt4gh decrypt --sk c4gh.sec.pem < dummy_data.c4gh > old-file.txt
+
+curl --cacert certs/ca.pem -H "Authorization: Bearer $token" "https://localhost:443/files/urn:neic:001-002" --output test-download.txt
+
+
+cmp --silent old-file.txt test-download.txt
+status=$?
+if [[ $status = 0 ]]; then
+    echo "Files are the same"
+else
+    echo "Files are different"
+fi
+
+
+# Test bad token
+
+token=$(curl "http://localhost:8000/tokens" | jq -r  '.[1]')
+
+## Test datasets endpoint
+
+check_empty_token=$(curl -o /dev/null -s -w "%{http_code}\n" -X GET -I --cacert certs/ca.pem -H "Authorization: Bearer $token" https://localhost:443/metadata/datasets)
+
+if [ "$check_empty_token" != "404" ]; then
+    echo "response for empty token is not 404"
+    echo "got: ${check_empty_token}"
+    exit 1
+fi
+
+echo "got correct response when token has no permissions"
