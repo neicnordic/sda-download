50ae3bb268cdc4a9eff96b550db18282d65c5bb9
Stefan Negru <stefan@blankdots.com>
2021-12-15 14:39:30 +0200

    reafactor auth package
    drop continue and streamline the visa checks

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -147,91 +147,113 @@ var GetPermissions = func(visas Visas) []string {
 	// Iterate visas
 	for _, v := range visas.Visa {
 
-		log.Debug("checking visa type")
 		// Check that visa is of type ControlledAccessGrants
-		unknownToken, err := jwt.Parse([]byte(v))
-		if err != nil {
-			log.Errorf("failed to parse visa, %s", err)
-			continue
+		if checkVisaType(v, "ControlledAccessGrants") {
+			// Check that visa is valid and return visa token
+			verifiedVisa, valid := validateVisa(v)
+			if valid {
+				// Parse the dataset name out of the value field
+				datasets = getDatasets(verifiedVisa, datasets)
+			}
 		}
-		unknownTokenVisaClaim := unknownToken.PrivateClaims()["ga4gh_visa_v1"]
-		unknownTokenVisa := Visa{}
-		unknownTokenVisaClaimJSON, err := json.Marshal(unknownTokenVisaClaim)
-		if err != nil {
-			log.Errorf("failed to parse unknown visa claim to JSON, %s, %s", err, unknownTokenVisaClaim)
-			continue
-		}
-		err = json.Unmarshal(unknownTokenVisaClaimJSON, &unknownTokenVisa)
-		if err != nil {
-			log.Errorf("failed to parse unknown visa claim JSON into struct, %s, %s", err, unknownTokenVisaClaimJSON)
-			continue
-		}
-		if unknownTokenVisa.Type != "ControlledAccessGrants" {
-			log.Debug("visa is not a ControlledAccessGrants, skip")
-			continue
-		}
-		log.Debug("visa type check passed")
 
-		log.Debug("start visa validation")
-		// Extract header from header.payload.signature
-		header, err := jws.Parse([]byte(v))
-		if err != nil {
-			log.Errorf("failed to parse visa header, %s", err)
-			continue
-		}
-		// Parse the jku key from header
-		o := OIDCDetails{
-			JWK: header.Signatures()[0].ProtectedHeaders().JWKSetURL(),
-		}
-		// Verify visa signature
-		verifiedVisa, err := VerifyJWT(o, v)
-		if err != nil {
-			log.Errorf("failed to validate visa, %s", err)
-			continue
-		}
-		// Validate visa claims, exp, iat, nbf
-		if err := jwt.Validate(verifiedVisa); err != nil {
-			log.Error("failed to validate visa")
-			continue
-		}
-		log.Debug("visa validated")
-		// Parse the dataset name out of the value field
-		visaClaim := verifiedVisa.PrivateClaims()["ga4gh_visa_v1"]
-		visa := Visa{}
-		visaClaimJSON, err := json.Marshal(visaClaim)
-		if err != nil {
-			log.Errorf("failed to parse visa claim to JSON, %s, %s", err, visaClaim)
-			continue
-		}
-		err = json.Unmarshal(visaClaimJSON, &visa)
-		if err != nil {
-			log.Errorf("failed to parse visa claim JSON into struct, %s, %s", err, visaClaimJSON)
-			continue
-		}
-		exists, err := database.CheckDataset(visa.Dataset)
-		if err != nil {
-			log.Debugf("visa contained dataset %s which doesn't exist in this instance, skip", visa.Dataset)
-			continue
-		}
-		if exists {
-			log.Debugf("checking dataset list for duplicates of %s", visa.Dataset)
-			// check that dataset name doesn't already exist in return list,
-			// we can get duplicates when using multiple AAIs
-			duplicate := false
-			for i := range datasets {
-				if datasets[i] == visa.Dataset {
-					duplicate = true
-					log.Debugf("found a duplicate: dataset %s is already found, skip", visa.Dataset)
-					continue
-				}
-			}
-			if !duplicate {
-				log.Debugf("no duplicates of dataset: %s, add dataset to list of permissions", visa.Dataset)
-				datasets = append(datasets, visa.Dataset)
-			}
-		}
 	}
 
-	log.Debugf("matched datasets, %s", datasets)
+	log.Debugf("matched datasets: %s", datasets)
+	return datasets
+}
+
+func checkVisaType(visa string, visaType string) bool {
+
+	log.Debug("checking visa type")
+
+	unknownToken, err := jwt.Parse([]byte(visa))
+	if err != nil {
+		log.Errorf("failed to parse visa, %s", err)
+		return false
+	}
+	unknownTokenVisaClaim := unknownToken.PrivateClaims()["ga4gh_visa_v1"]
+	unknownTokenVisa := Visa{}
+	unknownTokenVisaClaimJSON, err := json.Marshal(unknownTokenVisaClaim)
+	if err != nil {
+		log.Errorf("failed to parse unknown visa claim: %s to JSON, with error: %s", unknownTokenVisaClaim, err)
+		return false
+	}
+	err = json.Unmarshal(unknownTokenVisaClaimJSON, &unknownTokenVisa)
+	if err != nil {
+		log.Errorf("failed to parse unknown visa claim: %s to JSON, with error: %s", unknownTokenVisaClaim, err)
+		return false
+	}
+	if unknownTokenVisa.Type != visaType {
+		log.Debugf("visa is not of type: %s, skip", visaType)
+		return false
+	}
+	log.Debug("visa type check passed")
+
+	return true
+
+}
+
+func validateVisa(visa string) (jwt.Token, bool) {
+	log.Debug("start visa validation")
+	// Extract header from header.payload.signature
+	header, err := jws.Parse([]byte(visa))
+	if err != nil {
+		log.Errorf("failed to parse visa header, %s", err)
+		return nil, false
+	}
+	// Parse the jku key from header
+	o := OIDCDetails{
+		JWK: header.Signatures()[0].ProtectedHeaders().JWKSetURL(),
+	}
+	// Verify visa signature
+	verifiedVisa, err := VerifyJWT(o, visa)
+	if err != nil {
+		log.Errorf("failed to validate visa, %s", err)
+		return nil, false
+	}
+
+	// Validate visa claims, exp, iat, nbf
+	if err := jwt.Validate(verifiedVisa); err != nil {
+		log.Error("failed to validate visa")
+		return nil, false
+	}
+	log.Debug("visa validated")
+
+	return verifiedVisa, true
+}
+
+func getDatasets(parsedVisa jwt.Token, datasets []string) []string {
+	visaClaim := parsedVisa.PrivateClaims()["ga4gh_visa_v1"]
+	visa := Visa{}
+	visaClaimJSON, err := json.Marshal(visaClaim)
+	if err != nil {
+		log.Errorf("failed to parse visa claim to JSON, %s, %s", err, visaClaim)
+		return datasets
+	}
+	err = json.Unmarshal(visaClaimJSON, &visa)
+	if err != nil {
+		log.Errorf("failed to parse visa claim JSON into struct, %s, %s", err, visaClaimJSON)
+		return datasets
+	}
+	exists, err := database.CheckDataset(visa.Dataset)
+	if err != nil {
+		log.Debugf("visa contained dataset %s which doesn't exist in this instance, skip", visa.Dataset)
+		return datasets
+	}
+	if exists {
+		log.Debugf("checking dataset list for duplicates of %s", visa.Dataset)
+		// check that dataset name doesn't already exist in return list,
+		// we can get duplicates when using multiple AAIs
+		for i := range datasets {
+			if datasets[i] == visa.Dataset {
+				log.Debugf("found a duplicate: dataset %s is already found, skip", visa.Dataset)
+				return datasets
+			}
+		}
+		log.Debugf("no duplicates of dataset: %s, add dataset to list of permissions", visa.Dataset)
+		datasets = append(datasets, visa.Dataset)
+	}
+
 	return datasets
 }
