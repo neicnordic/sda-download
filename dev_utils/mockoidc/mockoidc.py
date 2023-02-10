dc69502df8d539703098129cfb6c4a733c96f503
Stefan Negru <stefan@blankdots.com>
2023-02-10 19:37:11 +0200

    jwk.dumps is deprecated adjust for that

--- dev_utils/mockoidc/mockoidc.py
+++ dev_utils/mockoidc/mockoidc.py
@@ -1,10 +1,7 @@
 """Mock OAUTH2 aiohttp.web server."""
 
 from aiohttp import web
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
-from authlib.jose import jwt, jwk
+from authlib.jose import jwt, RSAKey
 from typing import Tuple, Union
 import ssl
 from pathlib import Path
@@ -32,18 +29,8 @@ def _set_ssl() -> Union[ssl.SSLContext, None]:
 def _generate_token() -> Tuple:
     """Generate RSA Key pair to be used to sign token and the JWT Token itself."""
     global HTTP_PROTOCOL
-    private_key = rsa.generate_private_key(
-        public_exponent=65537, key_size=2048, backend=default_backend()
-    )
-    public_key = private_key.public_key().public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-    )
-    pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
+
+    key = RSAKey.generate_key(is_private=True)
     # we set no `exp` and other claims as they are optional in a real scenario these should be set
     # See available claims here: https://www.iana.org/assignments/jwt/jwt.xhtml
     # the important claim is the "authorities"
@@ -126,8 +113,8 @@ def _generate_token() -> Tuple:
         "jti": "d1d7b521-bd6b-433d-b2d5-3d874aab9d55",
     }
 
-    public_jwk = jwk.dumps(public_key, kty="RSA")
-    private_jwk = jwk.dumps(pem, kty="RSA")
+    public_jwk = key.as_dict(is_private=False)
+    private_jwk = dict(key)
 
     # token that contains demo dataset and trusted visas
     trusted_token = jwt.encode(header, trusted_payload, private_jwk).decode("utf-8")
